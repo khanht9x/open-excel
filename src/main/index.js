40a42e64c0039481752239a7fe3f63572730a6f9
khanht <khanht.dev@gmail.com>
2019-07-22 18:25:19 +0700

    update

---
+++ src/main/index.js
@@ -0,0 +1,84 @@
+import { app, BrowserWindow, dialog } from 'electron'
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+import { autoUpdater } from 'electron-updater'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 650,
+    useContentSize: true,
+    width: 1200
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+// app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+autoUpdater.logger = require('electron-log')
+autoUpdater.logger.transports.file.level = 'info'
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  console.log('update-downloaded lats quitAndInstall')
+
+  if (process.env.NODE_ENV === 'production') {
+    dialog.showMessageBox({
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }, (buttonIndex) => {
+      if (buttonIndex === 0) {
+        const isSilent = true
+        const isForceRunAfter = true
+        autoUpdater.quitAndInstall(isSilent, isForceRunAfter)
+      }
+    })
+  }
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') { autoUpdater.checkForUpdates() }
+  // autoUpdater.checkForUpdates();
+  createWindow()
+})
---
+++ src/renderer/components/OpenFile/OpenFile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="wrapper">
+    <p>Yunxi Auto</p>
+    <div class="version" style="position: fixed; right: 30px; bottom: 30px;">
+      <p><small>Version 1.01</small></p>
+      <p><small>093.595.0000</small></p>
+    </div>
+  </div>
+</template>
+
+<script>
+const path = require('path')
+export default {
+  name: 'open-file',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    open (link) {
+      this.$electron.shell.openExternal(link)
+    }
+  },
+  mounted () {
+    this.path = path.join(path.dirname(__dirname), '../../extraResources/yunxi-excel.xlsx')
+    this.open(this.path)
+  }
+}
+</script>
